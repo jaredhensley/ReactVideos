9e0c567e94cb822b7591582589fbedb81f7e42c1
Jared Hensley <jahensle85@me.com>
2019-06-23 16:27:18 -0500

    added video detail component and callback chain for passing currently selected video

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 import youtube from '../apis/youtube';
 
 class App extends React.Component {
 
     state = {
-        videos: []
+        videos: [],
+        selectedVideo: null
     }
 
     onTermSubmit = async (term) => {
@@ -18,14 +21,34 @@ class App extends React.Component {
         });
 
         this.setState({
-            videos: response.data.items
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
         });
+    };
+
+    onVideoSelect = (video) => {
+        this.setState({ selectedVideo: video });
+    }
+
+    componentDidMount() {
+        // default load
+        this.onTermSubmit('buildings');
     }
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit}/>
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo}></VideoDetail>
+                        </div>
+                        <div className="five wide column">
+                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
